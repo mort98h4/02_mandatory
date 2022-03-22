656b2a6f3c4803bad7ad25b03a92b3fe6021593e
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-03-22 14:32:26 +0100

    Created a log in form with frontend validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,47 @@
+"use strict";
+
+async function logIn() {
+    event.preventDefault();
+    const form = document.querySelector("#loginForm");
+    const userEmail = form.user_email;
+    const emailHint = document.querySelector("#emailHint");
+    const userPassword = form.user_password;
+    const passwordHint = document.querySelector("#passwordHint");
+    const emailStatus = userEmail.validity;
+    const passwordStatus = userPassword.validity; 
+
+    if (emailStatus.valueMissing) {
+        emailHint.style.display = "block";
+        emailHint.textContent = "Please enter your e-mail.";
+        return false;
+    } else if (emailStatus.typeMismatch || emailStatus.patternMismatch) {
+        emailHint.textContent = "Please enter a valid e-mail.";
+        emailHint.style.display = "block";
+        return false;
+    } else {
+        emailHint.textContent = "";
+        emailHint.style.display = "none";
+    }
+
+    console.log(passwordStatus);
+
+    if (passwordStatus.valueMissing) {
+        passwordHint.textContent = "Please enter your password.";
+        passwordHint.style.display = "block";
+        return false;
+    } else if (passwordStatus.patternMismatch) {
+        passwordHint.textContent = "Password must be at least 8 characters containing at least 1 uppercase letter, 1 lowercase letter and 1 number.";
+        passwordHint.style.display = "block";
+        userPassword.value = "";
+        return false;
+    } else {
+        passwordHint.textContent = "";
+        passwordHint.style.display = "none";
+    }
+    
+    form.submit();
+}
+
 async function tweet() {
     const form = event.target.form;
     console.log(form.tweet_text.value);
