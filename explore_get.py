871b6faaac1ad9ee9342817dc3daa592506f5baf
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-03-23 10:13:47 +0100

    Fetches the tweets from the database and passes them to the view

--- explore_get.py
+++ explore_get.py
@@ -5,8 +5,11 @@ import jwt
 ##############################
 @get("/explore")
 @get("/<language>/explore")
-# @view("explore")
+@view("explore")
 def _(language = "en"):
+    user = {}
+    tweets = []
+
     try:
         if f"{language}_server_error" not in g.ERRORS: language = "en"
         encoded_jwt = request.get_cookie("jwt")
@@ -18,16 +21,23 @@ def _(language = "en"):
                                         FROM sessions
                                         JOIN users
                                         WHERE users.user_id = ? 
-                                        AND sessions.session_id = ?""", (decoded_jwt['user_id'], decoded_jwt['session_id'])).fetchone()
-            print("#"*30)
-            print(decoded_jwt['session_id'])
-            print(user)
+                                        AND sessions.session_id = ?""", (decoded_jwt['user_id'], decoded_jwt['session_id'])).fetchone()            
             db.close()
-            return user
     except Exception as ex:
         print(ex)
         return g._SEND(500, g.ERRORS[f"{language}_server_error"])
+    
+    try: 
+        db = g._DB_CONNECT("database.sqlite")
+        tweets = db.execute("""SELECT tweets.tweet_id, tweets.tweet_text, tweets.tweet_image_src, tweets.tweet_created_at, tweets.tweet_created_at_date, tweets.tweet_updated_at, tweets.tweet_updated_at_date, tweets.user_id, users.user_handle, users.user_first_name, users.user_last_name, users.user_image_src
+                               FROM tweets
+                               JOIN users
+                               WHERE tweets.user_id = users.user_id
+                               ORDER BY tweet_created_at DESC""").fetchall()
         
-    
-
-    
+        return dict(user=user, tweets=tweets)
+    except Exception as ex:
+        print(ex)
+        return g._SEND(500, g.ERRORS[f"{language}_server_error"])
+    finally: 
+        db.close()
