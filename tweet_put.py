6aee0be9558ad8cc8b4808233d4be2a13eecc5ee
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-03-23 19:35:45 +0100

    Old image is deleted

--- tweet_put.py
+++ tweet_put.py
@@ -28,6 +28,7 @@ def _(language = "en", id=""):
     try:
         db = g._DB_CONNECT("database.sqlite")
         tweet = db.execute("SELECT * FROM tweets WHERE tweet_id = ?", (tweet_id,)).fetchone()
+        image_path = (tweet['tweet_image_src'])
         if not tweet: return g._SEND(204, "")
 
         for key in tweet.keys():
@@ -40,11 +41,7 @@ def _(language = "en", id=""):
             tweet_image_src, error = g._IS_TWEET_IMAGE(request.files.get("tweet_image_src"), language)
             if error: return g._SEND(400, error)
         else:
-            tweet_image_src = tweet['tweet_image_src']
-        if not tweet["tweet_image_src"] == tweet_image_src or tweet["tweet_image_src"] != "":
-            # REMEMBER TO DELETE OLD IMAGE
-            print(tweet_image_src)
-            print(tweet["tweet_image_src"])
+            tweet_image_src = ""
 
         tweet['tweet_text'] = tweet_text
         tweet['tweet_image_src'] = tweet_image_src
@@ -60,6 +57,7 @@ def _(language = "en", id=""):
                                 WHERE tweet_id = :tweet_id""", tweet).rowcount
         db.commit()
         if not counter: return g._SEND(204, "")
+        if image_path != "": os.remove(f"./images/{image_path}")
         return json.dumps(tweet)
     except Exception as ex:
         print(ex)
