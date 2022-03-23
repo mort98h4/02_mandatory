32416f15bee9d383bb196437db8346a9a770c65d
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-03-23 11:30:08 +0100

    Removed unused code

--- login_get.py
+++ login_get.py
@@ -7,23 +7,4 @@ import g
 @view("login")
 def _(language = "en"):
     if f"{language}_server_error" not in g.ERRORS: language = "en"
-
-    errors = {
-        "en": False,
-        "da": False
-    }
-    form_error = request.params.get("error")
-    user_email = request.params.get("user_email")
-
-    if form_error == "user_not_found": 
-        errors = {
-            "en": "The user does not exist.",
-            "da": "Brugeren findes ikke."
-        }
-    if form_error == "incorrect_password":
-        errors = {
-            "en": "The password was incorrect.",
-            "da": "Kodeordet var inkorrekt."
-        }
-
-    return dict(error=errors[language], user_email=user_email)
+    return
