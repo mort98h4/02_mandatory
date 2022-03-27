aa7bb2841baabf70ccd7c3efde7537fe2858217d
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-03-27 16:29:35 +0200

    Added some custom blue twitter colors

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   purge: ["../views/*.html"],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "twitter-blue1": "rgba(29, 155, 240, 1)",
+        "twitter-blue1-10": "rgba(29, 155, 240, .1)",
+        "twiiter-blue2": "rgba(26, 140, 216, 1)"
+      }
+    },
   },
   plugins: [],
 }
